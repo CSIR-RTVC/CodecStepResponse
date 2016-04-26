e2bfe5388de7c0443d5c156afb7574a2677a8e0e
miseri <ralf.globisch@gmail.com>
2016-04-26 09:12:50 +0200

    Added parsing of bitrates and switch points

--- src/apps/EvalCodecStepResponse/main.cpp
+++ src/apps/EvalCodecStepResponse/main.cpp
@@ -85,7 +85,7 @@ struct RateDescriptor
 std::vector<RateDescriptor> parseRateDescriptor(const std::string& sRatesDescriptor)
 {
   std::vector<RateDescriptor> rates;
-  std::vector<std::string> vSegments = StringTokenizer::tokenize(sRatesDescriptor, "|", true, true);
+  std::vector<std::string> vSegments = StringTokenizer::tokenize(sRatesDescriptor, ",", true, true);
   // only the last segment can not have a duration
   for (const std::string& sSegment : vSegments)
   {
@@ -150,7 +150,7 @@ void validateVideoCodecImpl(const std::string& sVideoCodecImpl)
   }
 }
 
-std::unique_ptr<IVideoCodecTransform> createAndInitialiseCodec(const std::string& sVideoCodec, const std::string& sVideoCodecImpl, uint32_t uiWidth, uint32_t uiHeight, double dFps, const std::vector<std::string>& videoCodecParams)
+std::unique_ptr<IVideoCodecTransform> createAndInitialiseCodec(const std::string& sVideoCodec, const std::string& sVideoCodecImpl, uint32_t uiWidth, uint32_t uiHeight, double dFps, const std::vector<std::string>& videoCodecParams, uint32_t uiInitialBitrateKbps)
 {
   std::unique_ptr<IVideoCodecTransform> pCodec;
   if (sVideoCodec == "H264")
@@ -216,6 +216,8 @@ std::unique_ptr<IVideoCodecTransform> createAndInitialiseCodec(const std::string
 
   if (pCodec)
   {
+    VLOG(2) << "Setting initial bitrate to " << uiInitialBitrateKbps << " kbps";
+    pCodec->setBitrate(uiInitialBitrateKbps);
     ec = pCodec->initialise();
     if (ec)
     {
@@ -246,6 +248,18 @@ std::unique_ptr<MediaSink> createMediaSink(const std::string& sVideoCodec, const
   return pMediaSink;
 }
 
+enum RateMode
+{
+  RATE_MODE_KBPS = 0,
+  RATE_MODE_BPP = 1
+};
+
+enum SwitchMode
+{
+  SWITCH_MODE_FRAME = 0,
+  SWITCH_MODE_TIME = 1
+};
+
 int main(int argc, char** argv)
 {
   // call any code here that needs to be called on application startup
@@ -261,6 +275,7 @@ int main(int argc, char** argv)
     uint32_t uiWidth = 0;
     uint32_t uiHeight = 0;
     double dFps = 0.0;
+    bool bRepeat = false;
     uint32_t uiLoopCount = 1;
     std::string sLogfile, sLogDir;
     std::string sVideoCodec, sVideoCodecImpl;
@@ -278,7 +293,8 @@ int main(int argc, char** argv)
         ("width,w", value<uint32_t>(&uiWidth)->required()->notifier(validateWidth), "Width")
         ("height,h", value<uint32_t>(&uiHeight)->required()->notifier(validateHeight), "Height")
         ("fps,f", value<double>(&dFps)->required()->notifier(validateFps), "FPS")
-        ("loop-count,c", value<uint32_t>(&uiLoopCount)->default_value(1), "Loop count. 0 = infinite.")
+        ("repeat,r", bool_switch(&bRepeat)->default_value(false), "Repeat source on eof.")
+        ("repeat-count,c", value<uint32_t>(&uiLoopCount)->default_value(1), "Number of repetitions. 0 = infinite.")
         ("video-codec", value<std::string>(&sVideoCodec)->required()->notifier(validateVideoCodec), "Codec: [h264,h265]")
         ("vc-impl", value<std::string>(&sVideoCodecImpl)->required()->notifier(validateVideoCodecImpl), "Codec: [openh264,x264,x265,vpp]")
         ("vc-param", value<std::vector<std::string>>(&videoCodecParams), "Video codec parameters")
@@ -308,10 +324,55 @@ int main(int argc, char** argv)
     google::SetLogDestination(google::GLOG_WARNING, (logPath.str() + ".WARNING").c_str());
     google::SetLogDestination(google::GLOG_ERROR, (logPath.str() + ".ERROR").c_str());
 
+    // convert to kbps as this is understood by encoders
+    // convert switch point to frame
+    std::vector<RateDescriptor> rates = parseRateDescriptor(sRateDescriptor);
+    std::vector<double> vKbps;
+    std::vector<uint32_t> vSwitchFrames = {0}; // first switch to bitrate is at the beginning
+    uint32_t uiPreviousSwitchFrame = 0;
+    double dFrameDuration = 1.0/dFps;
+    for (RateDescriptor rate : rates)
+    {
+      switch (uiRateMode)
+      {
+        case RATE_MODE_KBPS:
+        {
+          vKbps.push_back(rate.Rate);
+          break;
+        }
+        case RATE_MODE_BPP:
+        {
+          double dKbps = rate.Rate * uiWidth * uiHeight * dFps;
+          vKbps.push_back(dKbps);
+          break;
+        }
+      }
+      switch (uiSwitchMode)
+      {
+        case SWITCH_MODE_FRAME:
+        {
+          uint32_t uiNextSwitch = uiPreviousSwitchFrame + rate.Duration;
+          vSwitchFrames.push_back(uiNextSwitch);
+          uiPreviousSwitchFrame = uiNextSwitch;
+          break;
+        }
+        case SWITCH_MODE_TIME:
+        {
+          if (rate.Duration != -1.0)
+          {
+            uint32_t uiNextSwitch = uiPreviousSwitchFrame + (rate.Duration/dFrameDuration);
+            vSwitchFrames.push_back(uiNextSwitch);
+            uiPreviousSwitchFrame = uiNextSwitch;
+          }
+          break;
+        }
+      }
+    }
+
     boost::to_upper(sVideoCodec);
     boost::to_upper(sVideoCodecImpl);
 
-    std::unique_ptr<IVideoCodecTransform> pCodec = createAndInitialiseCodec(sVideoCodec, sVideoCodecImpl, uiWidth, uiHeight, dFps, videoCodecParams);
+    std::unique_ptr<IVideoCodecTransform> pCodec = createAndInitialiseCodec(sVideoCodec, sVideoCodecImpl, uiWidth, uiHeight, dFps, videoCodecParams, vKbps.at(0));
     if (!pCodec)
     {
       LOG(ERROR) << "Failed to create and initialise codec.";
@@ -325,17 +386,33 @@ int main(int argc, char** argv)
       return -1;
     }
 
-    media::YuvMediaSource yuvMediaSource(sYuvFile, uiWidth, uiHeight, uiLoopCount > 1, uiLoopCount);
-    int iCount = 0;
+    media::YuvMediaSource yuvMediaSource(sYuvFile, uiWidth, uiHeight, bRepeat, uiLoopCount);
 
+    int iCurrentFrame = 0;
+    uint32_t uiCurrentRateKbpsIndex = 0;
+    uint32_t uiCurrentSwitchFrameIndex = 0;
     auto start = std::chrono::steady_clock::now();
+
     while (yuvMediaSource.isGood())
     {
       std::vector<media::MediaSample> encodedSamples;
       std::vector<media::MediaSample> frame = yuvMediaSource.getNextAccessUnit();
       if (!frame.empty())
       {
-        ++iCount;
+        if ((uiCurrentSwitchFrameIndex < vSwitchFrames.size()) &&
+            (vSwitchFrames[uiCurrentSwitchFrameIndex] == iCurrentFrame) &&
+            (uiCurrentRateKbpsIndex < vKbps.size())
+            )
+        {
+          VLOG(2) << "Setting next bitrate to " << vKbps[uiCurrentRateKbpsIndex] << " kbps Current frame: " << iCurrentFrame;
+          boost::system::error_code ec = pCodec->setBitrate(vKbps[uiCurrentRateKbpsIndex++]);
+          if (ec)
+          {
+            LOG(WARNING) << "Failed to update bitrate to " << vKbps[uiCurrentRateKbpsIndex - 1] << "kbps";
+          }
+          ++uiCurrentSwitchFrameIndex;
+        }
+
         // encode
         uint32_t uiEncodedSize = 0;
         boost::system::error_code ec = pCodec->transform(frame, encodedSamples, uiEncodedSize);
@@ -349,12 +426,13 @@ int main(int argc, char** argv)
           // write to sink
           pMediaSink->writeAu(encodedSamples);
         }
+        ++iCurrentFrame;
       }
     }
     auto end = std::chrono::steady_clock::now();
     auto diff = end - start;
     auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(diff);
-    LOG(INFO) << "Read " << iCount << " frames in " << sYuvFile << " (" << elapsed_ms.count() << " ms)";
+    LOG(INFO) << "Read " << iCurrentFrame << " frames in " << sYuvFile << " (" << elapsed_ms.count() << " ms)";
   }
   catch (boost::exception& e)
   {
